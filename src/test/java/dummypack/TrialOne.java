bc128b4c72dbe767d0420ab5a0c8912bf757bdb6
Ugur Nurcan <ugur-n@yandex.com>
2021-11-17 18:25:25 -0500

    third update

--- src/test/java/dummypack/TrialOne.java
+++ src/test/java/dummypack/TrialOne.java
@@ -4,5 +4,6 @@ public class TrialOne {
     public static void main(String[] args) {
         System.out.println("Trial One");
         System.out.println("Master update");
+        System.out.println("here is a new update");
     }
 }
