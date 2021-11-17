55193decf9e62a9b73273253ac23fa32a30bfcac
Ugur Nurcan <ugur-n@yandex.com>
2021-11-17 18:12:23 -0500

    local first commit

--- src/test/java/dummypack/TrialOne.java
+++ src/test/java/dummypack/TrialOne.java
@@ -3,5 +3,6 @@ package dummypack;
 public class TrialOne {
     public static void main(String[] args) {
         System.out.println("Trial One");
+        System.out.println("this is local branch update");
     }
 }
