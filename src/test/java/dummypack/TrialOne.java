c109512376a7388dcfdb824d2d3151631e652104
Ugur Nurcan <ugur-n@yandex.com>
2021-11-17 18:13:19 -0500

    second master

--- src/test/java/dummypack/TrialOne.java
+++ src/test/java/dummypack/TrialOne.java
@@ -3,5 +3,6 @@ package dummypack;
 public class TrialOne {
     public static void main(String[] args) {
         System.out.println("Trial One");
+        System.out.println("Master update");
     }
 }
